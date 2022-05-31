b0a2fcbaef75bb689edbd0dcad4f73a5e4b407f0
unknown <willbeckcg@gmail.com>
2022-05-31 15:26:55 -0300

    chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import { ChakraProvider, extendTheme, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+
+import theme from '../styles/theme';
+
+const myTheme = extendTheme(theme);
+
+const GlobalStyle = ({ children }) => (
+  <>
+    <Head>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </Head>
+    <CSSReset />
+    <Global
+      styles={css`
+        html {
+          scroll-behavior: smooth;
+        }
+
+        #__next {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+      `}
+    />
+    {children}
+  </>
+);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={myTheme}> 
+        <GlobalStyle />
+        <Component {...pageProps} />      
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
