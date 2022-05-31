2f68c939e3011d678d8a3d2a398d21a6f904b341
unknown <willbeckcg@gmail.com>
2022-05-31 15:40:15 -0300

    finished

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 /* eslint-disable react/jsx-no-undef */
-import { Heading } from '@chakra-ui/react'
+import { Heading, Spinner } from '@chakra-ui/react'
 
 
 
 export default function Home() {
   return (
-    <Heading as='h1' size='4xl' noOfLines={1}>
+    <Heading as='h1' size='4xl' noOfLines={1} textAlign='center'>
      Estrutura Next e Lib ( Chakra Ui )
+     <Spinner
+  thickness='4px'
+  speed='0.65s'
+  emptyColor='gray.200'
+  color='blue.500'
+  size='xl'
+/>
   </Heading>
   )
 }
